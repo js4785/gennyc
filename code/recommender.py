4e8c15fc38430de21fa07e6b8bf0eaa9f07baa61
Kayvon Seif-Naraghi <kss2153@columbia.edu>
2018-04-23 19:39:09 -0400

    try again

--- code/recommender.py
+++ code/recommender.py
@@ -1,4 +1,5 @@
 import main
+import datetime
 
 ENV_DB = 'Dev'
 
@@ -53,13 +54,16 @@ class Recommend:
                 WHERE U.username='{}' AND
                     E.tag = U.tag AND
                     E1.eid = E.eid AND
-                    E1.lid = L.lid
+                    E1.lid = L.lid AND
+                    E1.start_date >= {}
+                ORDER by E1.start_date
                 """.format(
                         ENV_DB,
                         ENV_DB,
                         ENV_DB,
                         ENV_DB,
-                        self.user.username
+                        self.user.username,
+                        str(datetime.date.today())
                     )
 
         cursor.execute(query)
@@ -113,13 +117,16 @@ class GroupRecommend:
                     FROM {}.EventTags AS E, {}.UserTags AS U, {}.Events as E1, {}.Locations as L
                     WHERE E.tag = '{}' AND
                         E1.eid = E.eid AND
-                        E1.lid = L.lid
+                        E1.lid = L.lid AND
+                        E1.start_date > {}
+                    ORDER by E1.start_date
                     """.format(
                             ENV_DB,
                             ENV_DB,
                             ENV_DB,
                             ENV_DB,
-                            tag
+                            tag,
+                            str(datetime.date.today())
                         )
 
             cursor.execute(query)
